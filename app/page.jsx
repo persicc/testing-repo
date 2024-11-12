c457ae3323d80529e59ab0500359cc2c1eebd525
Luka Persic <persicluka@protonmail.com>
2024-11-12 03:13:41 +0100

    added shit

--- app/page.jsx
+++ app/page.jsx
@@ -3,20 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.page}>
-      <div className=""></div>
-      <div className="">Home</div>
-      <div className=""></div>
-      <div className="">Link</div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
+     
     </main>    
   );
 }
